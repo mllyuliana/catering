0e467b3605a42ba8bd970cb831f12e0f5ce684a5
mllyuliana <yulianam200348@gmail.com>
2025-06-04 18:14:35 +0300

    Update ALLOWED_HOSTS in settings.py

--- catering/settings.py
+++ catering/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['savore-catering.onrender.com']
 
 
 # Application definition
